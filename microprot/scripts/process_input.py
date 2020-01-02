62b1e88e706ae9160b2c41265326a05ff9926000
Pawel Szczerbiak <p.szczerbiak@gmail.com>
2020-01-02 12:21:02 +0100

    fasta input processing: add sorting by cluster size

--- microprot/scripts/process_input.py
+++ microprot/scripts/process_input.py
@@ -11,36 +11,33 @@ from microprot.scripts import process_fasta
 @click.option('--outfile', '-o', required=False, default=None,
               type=click.Path(resolve_path=True, readable=True, exists=False),
               help='Output protein sequence file in FASTA format.')
-@click.option('--sort_by_len', '-s', required=False, is_flag=True,
-              help='Sort sequences by length')
+@click.option('--sort_type', '-s', required=False, default=None, type=str,
+              help='seq: sort sequences by sequence length, '
+                   'clu: sort sequences by cluster size (shall be included '
+                   'in sequence header after last hash (#) sign).')
+@click.option('--sort_reversed', '-r', required=False, is_flag=True,
+              help='Sort sequences in reversed order.')
 @click.option('--min_len', '-m', required=False, default=1, type=int,
               help='Minimum sequence length to be included in output.')
 @click.option('--max_len', '-x', required=False, default=100000, type=int,
               help='Maximum sequence length to be included in output.')
-def _process_fasta_input(infile, outfile, sort_by_len, min_len, max_len):
+def _process_fasta_input(infile, outfile, sort_type, sort_reversed, min_len, max_len):
     fp = infile
     fp_name = os.path.splitext(fp)[0]
-
     fasta = process_fasta.extract_sequences(fp)
-    fasta = np.array(fasta)
-
     suffix = []
 
-    if sort_by_len is True:
-        suffix.append('_sorted')
-        s = [len(seq) for seq in fasta]
-        idx = sorted(range(len(s)), key=lambda k: s[k])
-        fasta = fasta[idx]
-        for i, seq in enumerate(fasta):
-            if len(seq) >= min_len:
-                output_fasta = fasta[i:]
-                break
-        for i, seq in enumerate(output_fasta):
-            if len(seq) > max_len:
-                output_fasta = output_fasta[:i]
-                break
-    else:
-        output_fasta = [seq for seq in fasta if min_len <= len(seq) <= max_len]
+    output_fasta = [seq for seq in fasta if min_len <= len(seq) <= max_len]
+
+    if sort_type in ["seq", "clu"]:
+        suffix.append('_sorted_' + sort_type)
+        if len(output_fasta) > 1:
+            if sort_type == "seq":
+                s = [len(seq) for seq in output_fasta]
+            else:
+                s = [int(seq.metadata["description"].split("#")[-1]) for seq in output_fasta]
+            idx = sorted(range(len(s)), key=lambda k: s[k], reverse=sort_reversed)
+            output_fasta = np.array(output_fasta)[idx]
 
     # checks if settings changed from default
     if min_len > 1:
